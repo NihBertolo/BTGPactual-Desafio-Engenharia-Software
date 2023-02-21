cd331b98dbd563ab9017ba67d19fbccb77eb841f
Nicole <nickbertolo@gmail.com>
2023-02-21 17:06:15 -0300

    Fix tests

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,23 @@
 #Prometheus Deploy
-kubectl create configmap prometheus-config --from-file=./deploy/prometheus/k8s/config/prometheus.yml
-kubectl apply -f ./deploy/prometheus/k8s/deployment.yaml
-kubectl apply -f ./deploy/prometheus/k8s/service.yaml
+#kubectl create namespace monitoring
+#kubectl create -f ./deploy/prometheus/k8s/manifests/clusterRole.yaml
+#kubectl create -f ./deploy/prometheus/k8s/manifests/config-map.yaml
+
+#kubectl create  -f ./deploy/prometheus/k8s/manifests/prometheus-deployment.yaml
+#kubectl create  -f ./deploy/prometheus/k8s/manifests/prometheus-service.yaml
+
+#printf "\n Prometheus Deployed!"
+
+#Initializing database
+docker run -d -p 27017:27017 --name test-mongo -v data-vol:/data/db -e MONGODB_INITDB_ROOT_USERNAME=desafio -e MONGODB_INITDB_ROOT_PASSWORD=engenharia mongo:latest
 
 #RabbitMQ Deploy
 kubectl apply -f ./deploy/rabbitmq/k8s/deployment.yaml
 kubectl apply -f ./deploy/rabbitmq/k8s/service.yaml
 
 #grafana Deploy
-kubectl apply -f ./deploy/grafana/k8s/deployment.yaml
-kubectl apply -f ./deploy/grafana/k8s/service.yaml
+#kubectl apply -f ./deploy/grafana/k8s/deployment.yaml
+#kubectl apply -f ./deploy/grafana/k8s/service.yaml
 
 #DeployMS
 for dir in $(find -type d); do
@@ -20,4 +28,4 @@ for dir in $(find -type d); do
   fi
 done
 
-echo 'Microservices Deployed! ;D'
+printf 'Microservices Deployed! ;D'
